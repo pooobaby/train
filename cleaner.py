ba9e11be41e874ff363873e9bea417dc33f448a7
POBABY <cuifaquan@hotmail.com>
2020-03-03 19:37:03 +0800

    countBySpeed()中修改计算方式的Bug

--- cleaner.py
+++ cleaner.py
@@ -12,14 +12,14 @@ from pymongo import MongoClient
 
 
 # noinspection PyPep8Naming
-class CleanData(object):
+class Cleaning(object):
     def __init__(self):
         self.client = MongoClient('localhost', port=27017)
         self.db = self.client.train
-        self.old = self.db.Train        # 旧的列车数据集合
+        self.old = self.db.Train
 
-        self.old_df = pd.DataFrame(self.old.find({}, {'_id': 0}))
-        self.old_df = self.old_df.drop_duplicates(subset='No', keep='first', inplace=False)   # 去除重复数据：8217
+        self.train_df = pd.DataFrame(self.old.find({}, {'_id': 0}))
+        self.train_df = self.train_df.drop_duplicates(subset='No', keep='first', inplace=False)
 
         self.type_dict = {'高速动车组': 'G', '普通动车组': 'D', '城际动车组': 'C', '快速': 'K',
                           '普快': '9', '直达特快': 'Z', '特快': 'T', '其他': 'O'}
@@ -32,8 +32,8 @@ class CleanData(object):
         :return:list: [(类型名称, 值)]
         """
         count_list = []
-        types = self.old_df.loc[:, 'type'].value_counts()
-        print('\n-- 数据库中共有%d次列车。' % self.old_df.shape[0])
+        types = self.train_df.loc[:, 'type'].value_counts()
+        print('\n-- 数据库中共有%d次列车。' % self.train_df.shape[0])
         for n, value in types.items():
             print('{0:>3}. {1:{3}<6}: {2:{3}<5}'.format(self.type_dict[n], n, value, chr(12288)))
             count_list.append((n, value))
@@ -45,7 +45,7 @@ class CleanData(object):
         enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列,同时列出数据和数据下标
         :return: list: [(车站名称, 值)]
         """
-        start_s = self.old_df.loc[:, 'start_s'].value_counts()
+        start_s = self.train_df.loc[:, 'start_s'].value_counts()
         start_s_list = []
         print('\n-- 共有%d个车站是始发站，始发列车最多的前20个车站:' % start_s.shape[0])
         for n, value in enumerate(start_s):
@@ -60,7 +60,7 @@ class CleanData(object):
         enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列,同时列出数据和数据下标
         :return: list: [(车站名称, 值)]
         """
-        end_s = self.old_df.loc[:, 'end_s'].value_counts()
+        end_s = self.train_df.loc[:, 'end_s'].value_counts()
         end_s_list = []
         print('\n-- 共有%d个车站是终点站，终到列车最多的前20个车站:' % end_s.shape[0])
         for n, value in enumerate(end_s):
@@ -75,7 +75,7 @@ class CleanData(object):
         尝试了多种方法，下面应该是最优化的算法，能不用遍历就不用
         :return: list: [(车站名称, 值)]
         """
-        station = self.old_df.key.tolist()
+        station = self.train_df.key.tolist()
         s_list = list(itertools.chain(*station))        # 用itertools库展开列表效率更高
         s_df = pd.DataFrame(s_list, columns=['s'])      # 将所有车站的列表转换成df，列名为's'
         s_df_dup = s_df.loc[:, 's'].value_counts().reset_index()   # 列表转成DataFrame后统计重复值、重设索引，
@@ -93,7 +93,7 @@ class CleanData(object):
         涉及到转换列表元素类型，groupby()函数的使用
         :return: list: [(里程区间, 值)]
         """
-        km = self.old_df.km.tolist()
+        km = self.train_df.km.tolist()
         km = list(map(int, km))     # 将列表中的元素转换为整数型
         km_list = []
         km_dict = {}
@@ -248,14 +248,13 @@ class CleanData(object):
         """
         -- 按运行速度对类型进行排序，并格式化输出
         计算过程中运行总时间包含了到站停车时间，因此不是很精确，待以后处理
-        2020-03-03:加入了计算每次列车停车时间的计算，
+        2020-03-03:加入了计算每次列车停车时间，修订计算方式的BUG
         :return: list: [(类型名称, 值)]
         """
         s_list = []
         speed = self.changePeriod()     # 调用转换运行时间函数
 
         speed['km'] = speed['km'].apply(pd.to_numeric)  # 将min列转换为整数型
-        speed['stop_time'] = ''
         for n in range(speed.shape[0]):         # 用遍历提取infos列中的数据，用以计算停车时间
             info = speed.iat[n, 7]
             stop_time_n = 0
@@ -269,13 +268,11 @@ class CleanData(object):
                     time_in = int(info[city]['st'].split(':')[0])*60 + int(info[city]['st'].split(':')[1])
                     stop_time_n += time_out - time_in
             speed.iat[n, 10] = speed.iat[n, 10] - stop_time_n
-
-        speed['speed'] = speed[['km', 'time']].apply(lambda x: x['km']/x['time']*60, axis=1)
-
         for n in self.type_dict:
             speed_n = speed[speed['type'].isin([n])]
-            speed_n_s = speed_n.speed.tolist()
-            n_speed = sum(speed_n_s)/len(speed_n_s)
+            km_n = speed_n.km.tolist()
+            speed_n_s = speed_n.time.tolist()
+            n_speed = sum(km_n)/sum(speed_n_s)*60
             s_list.append((n, n_speed))
         speed_list = sorted(s_list, key=lambda s: s[1], reverse=True)
         print('\n-- 各种类型的列车中，%s的速度最快，按速度对类型排序结果如下:' % speed_list[0][0])
@@ -289,7 +286,7 @@ class CleanData(object):
         -- 基础函数，用来根据period列的值进行分解处理，生成time列
         :return: df:包含time列
         """
-        change = self.old_df[['code', 'type', 'start_s', 'end_s', 'km', 'period', 'key', 'infos']].copy()
+        change = self.train_df[['code', 'type', 'start_s', 'end_s', 'km', 'period', 'key', 'infos']].copy()
         change['hour'] = change.period.apply(lambda x: x.split(':')[0])  # 分割period列生成hour列
         change['min'] = change.period.apply(lambda x: x.split(':')[1])  # 分割period列生成min列
         change['hour'] = change['hour'].apply(pd.to_numeric)  # 将hour列转换为整数型
@@ -302,7 +299,7 @@ class CleanData(object):
         -- 基础函数，用来根据km列的值进行排序，并将key列中的值生成key_n列
         :return: df:包含key_n列
         """
-        distance = self.old_df[['code', 'type', 'start_s', 'end_s', 'km', 'period', 'key']].copy()
+        distance = self.train_df[['code', 'type', 'start_s', 'end_s', 'km', 'period', 'key']].copy()
         distance['km'] = distance['km'].apply(pd.to_numeric)  # 将km列中的数值转换成整数型
         distance = distance.sort_values(by='km', ascending=False)  # 按km列降序排序
         distance['key_n'] = distance.key.apply(lambda x: len(x))  # 取key列中列表的长度，生成新列
@@ -312,18 +309,18 @@ class CleanData(object):
 
 
 def main():
-    clean_data = CleanData()
+    cleaning = Cleaning()
 
-    clean_data.countByType()
-    clean_data.countByStartStation()
-    clean_data.countByEndStation()
-    clean_data.countByStation()
-    clean_data.countByDistance()
-    clean_data.countByTopDistance()
-    clean_data.countByTypeDistance()
-    clean_data.countByPeriod()
-    clean_data.countByTopPeriod()
-    clean_data.countBySpeed()
+    # cleaning.countByType()
+    # cleaning.countByStartStation()
+    # cleaning.countByEndStation()
+    # cleaning.countByStation()
+    # cleaning.countByDistance()
+    # cleaning.countByTopDistance()
+    # cleaning.countByTypeDistance()
+    # cleaning.countByPeriod()
+    # cleaning.countByTopPeriod()
+    cleaning.countBySpeed()
 
 
 if __name__ == '__main__':
