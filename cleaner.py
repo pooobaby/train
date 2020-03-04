58b1596490f9b301b48b771207ee0627f362538c
POBABY <cuifaquan@hotmail.com>
2020-03-04 14:33:25 +0800

    用printResult()控制输出，新增changeXY()用于转换数据

--- cleaner.py
+++ cleaner.py
@@ -3,8 +3,6 @@
 
 # Copyright By Eric in 2020
 
-import pdb
-import json
 import itertools
 from itertools import groupby
 import pandas as pd
@@ -16,26 +14,173 @@ class Cleaning(object):
     def __init__(self):
         self.client = MongoClient('localhost', port=27017)
         self.db = self.client.train
-        self.old = self.db.Train
+        self.train = self.db.Train
+        self.station = self.db.StationGeo
 
-        self.train_df = pd.DataFrame(self.old.find({}, {'_id': 0}))
+        self.train_df = pd.DataFrame(self.train.find({}, {'_id': 0}))
         self.train_df = self.train_df.drop_duplicates(subset='No', keep='first', inplace=False)
+
+        self.station_df = pd.DataFrame(self.station.find({}, {'_id': 0}))
+        self.station_df = self.station_df.drop_duplicates(subset='name', keep='first', inplace=False)
 
         self.type_dict = {'高速动车组': 'G', '普通动车组': 'D', '城际动车组': 'C', '快速': 'K',
                           '普快': '9', '直达特快': 'Z', '特快': 'T', '其他': 'O'}
 
+    def printResult(self):
+        """
+        -- 打印输出模块
+        :return:
+        """
+        trains = self.train_df.shape[0]
+        start_stations = self.train_df.loc[:, 'start_s'].value_counts().shape[0]
+        end_stations = self.train_df.loc[:, 'end_s'].value_counts().shape[0]
+        kms = sum(list(map(int, self.train_df.km.tolist())))
+        hours = sum(self.changePeriod().time.tolist()) // 60
+
+        count_by_city = self.countByCity()[:20]
+        print('\n-- 全国火车站数量最多的前20个城市如下：')
+        for n in range(len(count_by_city)):
+            print('{0:>3}. {1:{3}<10}: {2:{3}<5}'
+                  .format(n+1, count_by_city[n][0], count_by_city[n][1], chr(12288)))
+
+        count_by_province = self.countByProvince()
+        print('\n-- 全国各省、市、自治区火车站数量分布如下：')
+        for n in range(len(count_by_province)):
+            print('{0:>3}. {1:{3}<10}: {2:{3}<5}'
+                  .format(n+1, count_by_province[n][0], count_by_province[n][1], chr(12288)))
+
+        count_by_type = self.countByType()
+        print('\n-- 数据库中共有%d次列车。' % trains)
+        for n in count_by_type:
+            print('{0:>3}. {1:{3}<6}: {2:{3}<5}'.format(self.type_dict[n[0]], n[0], n[1], chr(12288)))
+
+        count_by_start_Station = self.countByStartStation()
+        print('\n-- 共有%d个车站是始发站，始发列车最多的前20个车站:' % start_stations)
+        for n in range(20):
+            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'
+                  .format(n+1, count_by_start_Station[n][0], count_by_start_Station[n][1], chr(12288), ' '))
+
+        count_by_end_Station = self.countByEndStation()
+        print('\n-- 共有%d个车站是终点站，终到列车最多的前20个车站:' % end_stations)
+        for n in range(20):
+            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'
+                  .format(n+1, count_by_end_Station[n][0], count_by_end_Station[n][1], chr(12288), ' '))
+
+        count_by_station = self.countByStation()
+        print('\n-- 全国共有%d个车站，经过列车最多的前20个车站:' % len(count_by_station))
+        for n in range(1, 21):
+            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'
+                  .format(n, count_by_station[n-1][0], count_by_station[n-1][1], chr(12288), ' '))
+
+        count_by_distance = self.countByDistance()
+        print('\n-- 全国每天运行的旅客列车总里程{:,}公里，各里程区间运行的列车数量如下：'.format(kms))
+        for k in count_by_distance:
+            print('{0:{2}>12}Km : {1:{2}<6}'.format(k[0], k[1], ' '))
+
+        count_by_top_distance = self.countByTopDistance()
+        max_20_d = count_by_top_distance.iloc[:20, ]  # 切片取得前20行
+        min_20_d = count_by_top_distance.iloc[-20:, ]  # 切片取得后20行
+        print('\n-- 全国运行里程最长的列车是{}次，运行里程最长的前20次列车信息如下：'.format(max_20_d.iat[0, 0]))
+        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
+              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
+        for n in range(0, 20):
+            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
+                  .format(chr(12288), ' ', max_20_d.iat[n, 0], max_20_d.iat[n, 1], max_20_d.iat[n, 2],
+                          max_20_d.iat[n, 3], max_20_d.iat[n, 4], max_20_d.iat[n, 5], max_20_d.iat[n, 6],
+                          n + 1))
+
+        print('\n-- 全国运行里程最短的列车是{}次，运行里程最短的后20次列车信息如下：'.format(min_20_d.iat[-1, 0]))
+        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
+              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
+        for n in range(1, 21):
+            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
+                  .format(chr(12288), ' ', min_20_d.iat[-n, 0], min_20_d.iat[-n, 1], min_20_d.iat[-n, 2],
+                          min_20_d.iat[-n, 3], min_20_d.iat[-n, 4],
+                          min_20_d.iat[-n, 5], min_20_d.iat[-n, 6], n))
+
+        count_by_type_distance = self.countByTypeDistance()
+        for t in range(len(count_by_type_distance)):
+            print('\n-- 全国运行里程最长的{}列车是{}次，运行里程最长的前10次列车信息如下：'
+                  .format(count_by_type_distance[t].iat[0, 1], count_by_type_distance[t].iat[0, 0]))
+            print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
+                  .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
+            for n in range(count_by_type_distance[t].shape[0]):
+                print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
+                      .format(chr(12288), ' ', count_by_type_distance[t].iat[n, 0], count_by_type_distance[t].iat[n, 1],
+                              count_by_type_distance[t].iat[n, 2], count_by_type_distance[t].iat[n, 3],
+                              count_by_type_distance[t].iat[n, 4], count_by_type_distance[t].iat[n, 5],
+                              count_by_type_distance[t].iat[n, 6], n + 1))
+
+        count_by_period = self.countByPeriod()
+        print('\n-- 全国每天运行的旅客列车总时长为{:,}小时，各时长区间运行的列车数量如下：'.format(hours))
+        for p in count_by_period:
+            print('{0:{2}>12}h : {1:{2}<6}'.format(p[0], p[1], ' '))
+
+        count_by_top_period = self.countByTopPeriod()
+        max_20_p = count_by_top_period.iloc[:20, ]      # 切片取得前20行
+        min_20_p = count_by_top_period.iloc[-20:, ]     # 切片取得后20行
+        print('\n-- 全国运行时间最长的列车是{}次，运行时间最长的前20次列车信息如下：'.format(max_20_p.iat[0, 0]))
+        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
+              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
+        for n in range(0, 20):
+            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
+                  .format(chr(12288), ' ', max_20_p.iat[n, 0], max_20_p.iat[n, 1], max_20_p.iat[n, 2],
+                          max_20_p.iat[n, 3], max_20_p.iat[n, 4], max_20_p.iat[n, 5], max_20_p.iat[n, 7],
+                          n + 1))
+
+        print('\n-- 全国运行时间最短的列车是{}次，运行时间最短的后20次列车信息如下：'.format(min_20_p.iat[-1, 0]))
+        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
+              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
+        for n in range(1, 21):
+            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
+                  .format(chr(12288), ' ', min_20_p.iat[-n, 0], min_20_p.iat[-n, 1], min_20_p.iat[-n, 2],
+                          min_20_p.iat[-n, 3], min_20_p.iat[-n, 4],
+                          min_20_p.iat[-n, 5], min_20_p.iat[-n, 7], n))
+
+        count_by_speed = self.countBySpeed()
+        print('\n-- 各种类型的列车中，%s的速度最快，按速度对类型排序结果如下:' % count_by_speed[0][0])
+        for n, value in enumerate(count_by_speed):
+            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}Km/h'
+                  .format(n + 1, count_by_speed[n][0], int(count_by_speed[n][1]), chr(12288), ' '))
+
+        return
+
+    def countByCity(self):
+        """
+        -- 按城市统计车站数量
+        :return:list: [(城市名称, 值)]
+        """
+        count_list = []
+        cities = self.station_df.loc[:, 'city'].value_counts()
+        for n, value in cities.items():
+            if len(n) == 0:
+                continue
+            count_list.append((n, value))
+        return count_list
+
+    def countByProvince(self):
+        """
+        -- 按省份统计车站数量
+        :return:list: [(省份名称, 值)]
+        """
+        count_list = []
+        provinces = self.station_df.loc[:, 'province'].value_counts()
+        for n, value in provinces.items():
+            n = n.replace('省', '').replace('市', '').replace('自治区', '').replace('维吾尔', '').replace('回族', '')\
+                .replace('壮族', '').replace('特别行政区', '')
+            count_list.append((n, value))
+        return count_list
+
     def countByType(self):
         """
-        -- 按列车类型统计，并格式化输出
+        -- 按列车类型统计
         series.items()用于遍历series对象，同时列出索引和值。
         '{0} {1:{3}<6}: {2:{3}<5}'为格式化输出，中文字空格编码为chr(12288)，用来占位
         :return:list: [(类型名称, 值)]
         """
         count_list = []
         types = self.train_df.loc[:, 'type'].value_counts()
-        print('\n-- 数据库中共有%d次列车。' % self.train_df.shape[0])
         for n, value in types.items():
-            print('{0:>3}. {1:{3}<6}: {2:{3}<5}'.format(self.type_dict[n], n, value, chr(12288)))
             count_list.append((n, value))
         return count_list
 
@@ -47,10 +192,7 @@ class Cleaning(object):
         """
         start_s = self.train_df.loc[:, 'start_s'].value_counts()
         start_s_list = []
-        print('\n-- 共有%d个车站是始发站，始发列车最多的前20个车站:' % start_s.shape[0])
         for n, value in enumerate(start_s):
-            if n < 20:
-                print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'.format(n+1, start_s.index[n], value, chr(12288), ' '))
             start_s_list.append((start_s.index[n], value))
         return start_s_list
 
@@ -62,10 +204,7 @@ class Cleaning(object):
         """
         end_s = self.train_df.loc[:, 'end_s'].value_counts()
         end_s_list = []
-        print('\n-- 共有%d个车站是终点站，终到列车最多的前20个车站:' % end_s.shape[0])
         for n, value in enumerate(end_s):
-            if n < 20:
-                print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'.format(n+1, end_s.index[n], value, chr(12288), ' '))
             end_s_list.append((end_s.index[n], value))
         return end_s_list
 
@@ -81,10 +220,6 @@ class Cleaning(object):
         s_df_dup = s_df.loc[:, 's'].value_counts().reset_index()   # 列表转成DataFrame后统计重复值、重设索引，
         s_df_turn = s_df_dup.T.values.tolist()       # 使用T来转置数据，也就是行列转换
         station_list = list(zip(s_df_turn[0], s_df_turn[1]))      # zip()可以把两个列表转换为嵌套元组的列表
-        print('\n-- 全国共有%d个车站，经过列车最多的前20个车站:' % len(station_list))
-        for n in range(1, 21):
-            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}'
-                  .format(n, station_list[n-1][0], station_list[n-1][1], chr(12288), ' '))
         return station_list
 
     def countByDistance(self):
@@ -112,37 +247,15 @@ class Cleaning(object):
         km_list.append(('1500-1999', sum_15_20))
         km_list.append(('2000-2999', sum_20_30))
         km_list.append(('>3000', sum_30))
-        print('\n-- 全国每天运行的旅客列车总里程{:,}公里，各里程区间运行的列车数量如下：'.format(sum(km)))
-        for k in km_list:
-            print('{0:{2}>12}Km : {1:{2}<6}'.format(k[0], k[1], ' '))
         return km_list
 
     def countByTopDistance(self):
         """
-        -- 按列车运行里程排序，格式化输出前20名、后20名
+        -- 按列车运行里程排序
         涉及将df列中的数值转换类型，根据某一列的值进行处理生成新列，
         :return: df: 按运行里程排序后的df
         """
         km_top = self.sortDistance()        # 调用里程排序函数
-        max_top_20 = km_top.iloc[:20, ]     # 切片取得前20行
-        min_top_20 = km_top.iloc[-20:, ]    # 切片取得后20行
-
-        print('\n-- 全国运行里程最长的列车是{}次，运行里程最长的前20次列车信息如下：'.format(max_top_20.iat[0, 0]))
-        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
-              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
-        for n in range(0, 20):
-            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
-                  .format(chr(12288), ' ', max_top_20.iat[n, 0], max_top_20.iat[n, 1], max_top_20.iat[n, 2],
-                          max_top_20.iat[n, 3],  max_top_20.iat[n, 4], max_top_20.iat[n, 5], max_top_20.iat[n, 6], n+1))
-
-        print('\n-- 全国运行里程最短的列车是{}次，运行里程最短的后20次列车信息如下：'.format(min_top_20.iat[-1, 0]))
-        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
-              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
-        for n in range(1, 21):
-            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
-                  .format(chr(12288), ' ', min_top_20.iat[-n, 0], min_top_20.iat[-n, 1], min_top_20.iat[-n, 2],
-                          min_top_20.iat[-n, 3],  min_top_20.iat[-n, 4],
-                          min_top_20.iat[-n, 5], min_top_20.iat[-n, 6], n))
         return km_top
 
     def countByTypeDistance(self):
@@ -152,20 +265,11 @@ class Cleaning(object):
         """
         type_d_list = []
         type_d = self.sortDistance()        # 调用里程排序函数
-
         for t in self.type_dict:
             type_n = type_d[type_d['type'].isin([t])]
             type_n = type_n.sort_values(by='km', ascending=False)
             max_type_n = type_n.iloc[:10]
             type_d_list.append(max_type_n)
-            print('\n-- 全国运行里程最长的{}列车是{}次，运行里程最长的前10次列车信息如下：'.format(t, max_type_n.iat[0, 0]))
-            print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
-                  .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
-            for n in range(max_type_n.shape[0]):
-                print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
-                      .format(chr(12288), ' ', max_type_n.iat[n, 0], max_type_n.iat[n, 1], max_type_n.iat[n, 2],
-                              max_type_n.iat[n, 3], max_type_n.iat[n, 4], max_type_n.iat[n, 5], max_type_n.iat[n, 6],
-                              n + 1))
         return type_d_list
 
     def countByPeriod(self):
@@ -203,17 +307,13 @@ class Cleaning(object):
         period_list.append(('24-36', sum_24_36))
         period_list.append(('36-48', sum_36_48))
         period_list.append(('>48', sum_48))
-        print('\n-- 全国每天运行的旅客列车总时长为{:,}小时，各时长区间运行的列车数量如下：'
-              .format(sum(periods)//60))
-        for p in period_list:
-            print('{0:{2}>12}h : {1:{2}<6}'.format(p[0], p[1], ' '))
         return period_list
 
     def countByTopPeriod(self):
         """
-        -- 按列车运行时间排序，格式化输出前20名、后20名
+        -- 按列车运行时间排序
         涉及将df列中的数值转换类型，根据某一列的值进行处理生成新列，
-        :return: list[df, df]: [最长运行时间Top_20，最短运行时间Top_20]
+        :return: df: 列车运行时间排序后的df
         """
         period_top = self.changePeriod()        # 调用转换运行时间函数
 
@@ -221,28 +321,7 @@ class Cleaning(object):
         period_top['key_n'] = period_top.key.apply(lambda x: len(x))  # 取key列中列表的长度，生成新列
         period_top = period_top.drop(columns=['key', 'hour', 'min', 'time'])  # 删除key列
         period_top = period_top.drop_duplicates(subset='km', keep='first')  # 删除重复行
-        max_top_20 = period_top.iloc[:20, ]  # 切片取得前20行
-        min_top_20 = period_top.iloc[-20:, ]  # 切片取得后20行
-
-        print('\n-- 全国运行时间最长的列车是{}次，运行时间最长的前20次列车信息如下：'.format(max_top_20.iat[0, 0]))
-        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
-              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
-        for n in range(0, 20):
-            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
-                  .format(chr(12288), ' ', max_top_20.iat[n, 0], max_top_20.iat[n, 1], max_top_20.iat[n, 2],
-                          max_top_20.iat[n, 3], max_top_20.iat[n, 4], max_top_20.iat[n, 5], max_top_20.iat[n, 7],
-                          n + 1))
-
-        print('\n-- 全国运行时间最短的列车是{}次，运行时间最短的后20次列车信息如下：'.format(min_top_20.iat[-1, 0]))
-        print('{9:{1}>3}  {2:{1}^12}{3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^4} {7:{1}^6} {8:{1}^6}'
-              .format(chr(12288), ' ', '车次', '类型', '始发站', '终点站', '里程', '时间', '站点', ' '))
-        for n in range(1, 21):
-            print('{9:{1}>3}. {2:{1}^12} {3:{0}^7} {4:{0}^8} {5:{0}^8} {6:{1}^6} {7:{1}^8} {8:{1}^6}'
-                  .format(chr(12288), ' ', min_top_20.iat[-n, 0], min_top_20.iat[-n, 1], min_top_20.iat[-n, 2],
-                          min_top_20.iat[-n, 3], min_top_20.iat[-n, 4],
-                          min_top_20.iat[-n, 5], min_top_20.iat[-n, 7], n))
-        top_20 = [max_top_20, min_top_20]
-        return top_20
+        return period_top
 
     def countBySpeed(self):
         """
@@ -272,13 +351,9 @@ class Cleaning(object):
             speed_n = speed[speed['type'].isin([n])]
             km_n = speed_n.km.tolist()
             speed_n_s = speed_n.time.tolist()
-            n_speed = sum(km_n)/sum(speed_n_s)*60
+            n_speed = int(sum(km_n)/sum(speed_n_s)*60)
             s_list.append((n, n_speed))
         speed_list = sorted(s_list, key=lambda s: s[1], reverse=True)
-        print('\n-- 各种类型的列车中，%s的速度最快，按速度对类型排序结果如下:' % speed_list[0][0])
-        for n, value in enumerate(speed_list):
-            print('{0:{4}>3}. {1:{3}<6}: {2:{4}<5}Km/h'
-                  .format(n + 1, speed_list[n][0], int(speed_list[n][1]), chr(12288), ' '))
         return speed_list
 
     def changePeriod(self):
@@ -307,20 +382,19 @@ class Cleaning(object):
         distance = distance.drop_duplicates(subset='km', keep='first')  # 删除重复行
         return distance
 
+    @staticmethod
+    def changeXY(data):
+        x_data, y_data = [], []
+        for n in data:
+            x_data.append(n[0])
+            y_data.append(n[1])
+        result_list = [x_data, y_data]
+        return result_list
+
 
 def main():
     cleaning = Cleaning()
-
-    # cleaning.countByType()
-    # cleaning.countByStartStation()
-    # cleaning.countByEndStation()
-    # cleaning.countByStation()
-    # cleaning.countByDistance()
-    # cleaning.countByTopDistance()
-    # cleaning.countByTypeDistance()
-    # cleaning.countByPeriod()
-    # cleaning.countByTopPeriod()
-    cleaning.countBySpeed()
+    cleaning.printResult()
 
 
 if __name__ == '__main__':
